2f08262a213c17f9066f7d43e821e70e612af8ef
LAPTOP-53MB4V4Q\Walter_Ruganzu <walter@laboremus.no>
2021-08-17 15:13:22 +0300

    Refactoring Assignment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import ExpenseList from "./components/ExpenseList";
+
 
 function App() {
+  const expense = [
+    {id: 'e1', title: 'Car Insurance', amount: 294, date: new Date(2020, 7, 14)},
+    {id: 'e2', title: 'Toilet Paper', amount: 20, date: new Date(2021, 2, 28)},
+    {id: 'e3', title: 'Food', amount: 294, date: new Date(2020, 5, 4)},
+    {id: 'e4', title: 'Health Insurance', amount: 294, date: new Date(2020, 9, 24)},
+  ];
+
   return (
     <div>
       <h2>Lets get started.</h2> 
-      <p>This is also visible.</p>
+      <ExpenseList items={expense}/>
     </div>
   );
 }
