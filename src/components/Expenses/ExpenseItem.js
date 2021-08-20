dc6718045ee64aeabac170d0b6067d9a9a7cd033
LAPTOP-53MB4V4Q\Walter_Ruganzu <walter@laboremus.no>
2021-08-21 00:52:29 +0300

    Button Intro

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,27 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 
 
 function ExpenseItem(props) {
-    
+
+    let title = props.title;
+
+    const clickHandler = () => {
+        title = 'Updated';
+        console.log(title);
+    };
 
    
     return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
     </Card>
     );
 }
