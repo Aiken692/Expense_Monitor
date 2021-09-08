e2338503bfaa6c3260c9d6973a34b8dd3a9e5ae3
Aiken692 <rwalter692@gmail.com>
2021-09-08 16:50:23 +0300

    NewFilterrs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React, {useState} from "react";
 import ExpenseList from "./components/Expenses/ExpenseList";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
-  const expense = [
-    {id: 'e1', title: 'Car Insurance', amount: 294, date: new Date(2020, 7, 14)},
-    {id: 'e2', title: 'Toilet Paper', amount: 20, date: new Date(2021, 2, 28)},
-    {id: 'e3', title: 'Food', amount: 294, date: new Date(2020, 5, 4)},
-    {id: 'e4', title: 'Health Insurance', amount: 294, date: new Date(2020, 9, 24)},
-  ];
+const Dummy_Expense = [
+  {id: 'e1', title: 'Car Insurance', amount: 294, date: new Date(2020, 7, 14)},
+  {id: 'e2', title: 'Toilet Paper', amount: 20, date: new Date(2021, 2, 28)},
+  {id: 'e3', title: 'Food', amount: 294, date: new Date(2020, 5, 4)},
+  {id: 'e4', title: 'Health Insurance', amount: 294, date: new Date(2020, 9, 24)},
+];
 
+
+function App() {
+  const [expense, setExpense] = useState(Dummy_Expense);
+ 
   const addExpenseHandler = expense => {
     console.log('In App.js');
     console.log(expense);
--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -23,3 +23,4 @@ const ExpenseFilter = (props) => {
 }
 
 export default ExpenseFilter
+ 
